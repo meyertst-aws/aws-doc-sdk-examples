0e99c27a3235a2d998b1c164f20bc51840245d6d
Steven Meyer <meyertst@amazon.com>
2022-08-23 11:13:28 -0400

    added all files as duplicate

---
+++ cpp/example_code_2/general/main4.cpp
@@ -0,0 +1,15 @@
+// Copyright Amazon.com, Inc. or its affiliates.All Rights Reserved. 
+// SPDX - License - Identifier: Apache - 2.0
+
+// snippet-start:[general.cpp.starter.main]
+
+#include <iostream>
+
+
+int main()
+{
+    char* test = "some string";
+    std::cout << "test" << std::endl;
+    return 0;
+}
+// snippet-end:[general.cpp.starter.main]
