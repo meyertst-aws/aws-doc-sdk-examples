220274c7cc2fffe1c1f6ec5f0911de861aebdbf3
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-11-10 15:32:41 -0500

    working on cloudformation

--- cpp/example_code/medical-imaging/health_imaging_scout.cpp
+++ cpp/example_code/medical-imaging/health_imaging_scout.cpp
@@ -16,5 +16,186 @@
 
 #include <aws/core/Aws.h>
 #include <aws/cloudformation/CloudFormationClient.h>
+#include <aws/cloudformation/model/CreateStackRequest.h>
+#include <aws/cloudformation/model/DeleteStackRequest.h>
+#include <aws/cloudformation/model/DescribeStacksRequest.h>
+#include <aws/core/utils/UUID.h>
+#include <fstream>
 
-bool CreateS3BucketAndRole(Aws::Iam::IamClient &iamClient, Aws::String bucketName, Aws::String roleName)
+const char STACK_NAME[] = "health-imaging-scout";
+
+bool waitStackCreated(Aws::CloudFormation::CloudFormationClient &cloudFormationClient, const std::string &stackName)
+{
+    Aws::CloudFormation::Model::DescribeStacksRequest describeStacksRequest;
+    describeStacksRequest.SetStackName(stackName);
+    Aws::CloudFormation::Model::StackStatus stackStatus = Aws::CloudFormation::Model::StackStatus::CREATE_IN_PROGRESS;
+
+    while (stackStatus == Aws::CloudFormation::Model::StackStatus::CREATE_IN_PROGRESS)
+    {
+        stackStatus = Aws::CloudFormation::Model::StackStatus::NOT_SET;
+        auto outcome = cloudFormationClient.DescribeStacks(describeStacksRequest);
+        if (outcome.IsSuccess())
+        {
+            const auto& stacks = outcome.GetResult().GetStacks();
+            if (!stacks.empty())
+            {
+                const auto &stack = stacks[0];
+                stackStatus = stack.GetStackStatus();
+                if (stackStatus != Aws::CloudFormation::Model::StackStatus::CREATE_IN_PROGRESS  &&
+                    stackStatus != Aws::CloudFormation::Model::StackStatus::CREATE_COMPLETE)
+                {
+                    std::cerr << "Failed to create stack because " << stack.GetStackStatusReason() << std::endl;
+                }
+
+            }
+
+
+        }
+    }
+
+    if (stackStatus == Aws::CloudFormation::Model::StackStatus::CREATE_COMPLETE)
+    {
+        std::cout << "Stack creation completed." << std::endl;
+    }
+
+    return stackStatus == Aws::CloudFormation::Model::StackStatus::CREATE_COMPLETE;
+}
+
+bool waitStackDeleted(Aws::CloudFormation::CloudFormationClient &cloudFormationClient, const std::string &stackName)
+{
+    Aws::CloudFormation::Model::DescribeStacksRequest describeStacksRequest;
+    describeStacksRequest.SetStackName(stackName);
+    Aws::CloudFormation::Model::StackStatus stackStatus = Aws::CloudFormation::Model::StackStatus::DELETE_IN_PROGRESS;
+
+    while (stackStatus == Aws::CloudFormation::Model::StackStatus::DELETE_IN_PROGRESS)
+    {
+        stackStatus = Aws::CloudFormation::Model::StackStatus::NOT_SET;
+        auto outcome = cloudFormationClient.DescribeStacks(describeStacksRequest);
+        if (outcome.IsSuccess())
+        {
+            const auto& stacks = outcome.GetResult().GetStacks();
+            if (!stacks.empty())
+            {
+                const auto &stack = stacks[0];
+                stackStatus = stack.GetStackStatus();
+                if (stackStatus != Aws::CloudFormation::Model::StackStatus::DELETE_IN_PROGRESS  &&
+                    stackStatus != Aws::CloudFormation::Model::StackStatus::DELETE_COMPLETE)
+                {
+                    std::cerr << "Failed to delete stack because " << stack.GetStackStatusReason() << std::endl;
+                }
+
+            }
+            else {
+                stackStatus = Aws::CloudFormation::Model::StackStatus::DELETE_COMPLETE;
+            }
+
+        }
+        else{
+            auto&  error = outcome.GetError();
+            if (error.GetResponseCode() == Aws::Http::HttpResponseCode::BAD_REQUEST &&
+            (outcome.GetError().GetMessage().find("does not exist") != std::string::npos))
+            {
+                stackStatus = Aws::CloudFormation::Model::StackStatus::DELETE_COMPLETE;
+            }
+            else {
+                std::cerr << "Failed to describe stack. "
+                          << outcome.GetError().GetMessage() << std::endl;
+            }
+        }
+    }
+
+    if (stackStatus == Aws::CloudFormation::Model::StackStatus::DELETE_COMPLETE)
+    {
+        std::cout << "Stack deletion completed." << std::endl;
+    }
+
+    return stackStatus == Aws::CloudFormation::Model::StackStatus::DELETE_COMPLETE;
+}
+
+
+
+bool CreateImportStack(Aws::CloudFormation::CloudFormationClient &cloudFormationClient)
+{
+     Aws::CloudFormation::Model::CreateStackRequest createStackRequest;
+    createStackRequest.SetStackName(STACK_NAME);
+    createStackRequest.SetOnFailure(Aws::CloudFormation::Model::OnFailure::DELETE_)
+    std::ifstream ifstream("/Users/meyertst/Development/aws-doc-sdk-examples/cpp/example_code/medical-imaging/CfnImportBucketTemplate.json");
+
+    if (!ifstream)
+    {
+        std::cerr << "Failed to open file" << std::endl;
+        return "";
+    }
+
+    std::stringstream stringstream;
+    stringstream << ifstream.rdbuf();
+    std::string templateBody = stringstream.str();
+
+    std::string bucketName = "health-imaging-import-";
+    bucketName += (Aws::Utils::UUID::RandomUUID());
+    if (bucketName.length() > 63)
+    {
+        bucketName = bucketName.substr(0, 63);
+    }
+
+
+
+    std::transform(bucketName.begin(), bucketName.end(), bucketName.begin(),
+                   [](unsigned char c){ return std::tolower(c); });
+
+    std::string bucketTag("BucketName");
+    std::string policyName = "health-imaging-import-bucket-";
+    policyName += (Aws::Utils::UUID::RandomUUID());
+    createStackRequest.SetParameters(
+            { Aws::CloudFormation::Model::Parameter().WithParameterKey("BucketName").WithParameterValue(bucketName),
+              Aws::CloudFormation::Model::Parameter().WithParameterKey("PolicyName").WithParameterValue(policyName)});
+    createStackRequest.SetCapabilities({Aws::CloudFormation::Model::Capability::CAPABILITY_IAM});
+
+    createStackRequest.SetTemplateBody(templateBody);
+    auto outcome = cloudFormationClient.CreateStack(createStackRequest);
+    bool result = false;
+    if (outcome.IsSuccess())
+    {
+        std::cout << "Stack creation initiated." << std::endl;
+        result = waitStackCreated(cloudFormationClient, STACK_NAME);
+    }
+    else {
+        std::cerr << "Failed to create stack" << outcome.GetError().GetMessage()
+                  << std::endl;
+    }
+
+    return result;
+}
+
+bool deleteStack(Aws::CloudFormation::CloudFormationClient &cloudFormationClient, const std::string &stackName)
+{
+    Aws::CloudFormation::Model::DeleteStackRequest deleteStackRequest;
+    deleteStackRequest.SetStackName(stackName);
+    auto outcome = cloudFormationClient.DeleteStack(deleteStackRequest);
+    bool result = false;
+    if (outcome.IsSuccess())
+    {
+        std::cout << "Stack deletion initiated." << std::endl;
+        result = waitStackDeleted(cloudFormationClient, stackName);
+    }
+    else {
+        std::cerr << "Failed to delete stack" << outcome.GetError().GetMessage()
+                << std::endl;
+    }
+
+    return result;
+}
+
+
+int main(int argc, char **argv) {
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+    {
+        Aws::CloudFormation::CloudFormationClient cloudFormationClient;
+        CreateImportStack(cloudFormationClient);
+        deleteStack(cloudFormationClient, STACK_NAME);
+    }
+    Aws::ShutdownAPI(options);
+
+    return 0;
+}
